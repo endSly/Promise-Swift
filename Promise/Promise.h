4e631c47447704d60298f435939b9c712a58faf2
Endika Gutiérrez <me@endika.net>
2014-06-16 22:56:12 +0200

    Added Xcode Project

---
+++ Promise/Promise.h
@@ -0,0 +1,17 @@
+//
+//  Promise.h
+//  Promise
+//
+//  Created by Endika Gutiérrez Salas on 16/06/14.
+//  Copyright (c) 2014 Endika Gutiérrez Salas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promise.
+FOUNDATION_EXPORT double PromiseVersionNumber;
+
+//! Project version string for Promise.
+FOUNDATION_EXPORT const unsigned char PromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promise/PublicHeader.h>
